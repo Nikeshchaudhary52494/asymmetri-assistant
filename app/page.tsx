61ee7387d51c9fb5f6e737d60e57e5a3fa723a48
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2026-02-16 22:41:15 +0530

    repo init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-black flex justify-center items-center h-screen">
+      <h1 className="font-bold text-5xl">AI ASSISTANT</h1>
+    </div>
+  );
+}
